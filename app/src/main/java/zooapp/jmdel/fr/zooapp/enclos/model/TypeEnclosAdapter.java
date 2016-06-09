97f7d7399e126b266fae9ca21d3208c88ce9fd4e
XavierLugol <xavier.lugol@laposte.net>
2016-06-09 11:16:24 +0200

    Apres diverses maj

--- app/src/main/java/zooapp/jmdel/fr/zooapp/enclos/model/TypeEnclosAdapter.java
+++ app/src/main/java/zooapp/jmdel/fr/zooapp/enclos/model/TypeEnclosAdapter.java
@@ -1,28 +1,7 @@
 package zooapp.jmdel.fr.zooapp.enclos.model;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-
-import zooapp.jmdel.fr.zooapp.model.GenericArrayAdapter;
-
 /**
- * Created by hb on 08/06/2016.
+ * Created by hb on 09/06/2016.
  */
-public class TypeEnclosAdapter extends GenericArrayAdapter<String> {
-
-    public TypeEnclosAdapter(Context context, ArrayList<String> objects)  {
-        super(context, objects);
-    }
-
-    public void drawText(TextView textView, String typeEnclos) {
-        textView.setText(typeEnclos);
-    }
-
+public class TypeEnclosAdapter {
 }
