4c48b1978ccd4a7d37a44ad676ad61e15f86840d
jean-michel Delorme <jm.delorme@ymail.com>
2016-06-22 14:41:13 +0200

    Clean unexpected google api import

--- app/src/main/java/zooapp/jmdel/fr/zooapp/activity/food/FoodActivity.java
+++ app/src/main/java/zooapp/jmdel/fr/zooapp/activity/food/FoodActivity.java
@@ -9,20 +9,10 @@ import android.view.View;
 import android.widget.ListView;
 import java.util.ArrayList;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
+
 
 import zooapp.jmdel.fr.zooapp.R;
 import zooapp.jmdel.fr.zooapp.model.Food.Food;
@@ -31,11 +21,7 @@ import zooapp.jmdel.fr.zooapp.model.Food.FoodManager;
 
 public class FoodActivity extends ListActivity {
     ArrayList<Food> foodlist = FoodManager.getInstance().getFoodList();
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,9 +43,6 @@ public class FoodActivity extends ListActivity {
                 startActivity(intent);
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -79,44 +62,6 @@ public class FoodActivity extends ListActivity {
         setListAdapter(foodAdapter);
 
     }
+    
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Food Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://zooapp.jmdel.fr.zooapp.activity.food/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Food Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://zooapp.jmdel.fr.zooapp.activity.food/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
